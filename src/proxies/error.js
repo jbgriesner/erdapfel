80c9333191f7bc6971ef287fc1a61d89a9471cf4
jlaville <j.laville@qwant.com>
2018-07-11 17:20:05 +0200

    WIP error supervisor

--- src/proxies/error.js
+++ src/proxies/error.js
@@ -4,13 +4,25 @@ import Ajax from '../libs/ajax'
 let systemConfig = nconf.get().system
 let servicesConfig = nconf.get().services
 
-function Error() {}
+function Error() {
+  this.networkAvailable = true
+  this.storeAvailable = true
+}
 
 if(!window.__error) {
   window.__error = new Error()
 }
 
-Error.prototype.showUserError = function(message) {
+Error.prototype.setNetworkError = function(status) {
+  fire('error_h', message)
+  this.log(message)
+}
+
+Error.prototype.setStoreError = function (status) {
+  
+}
+
+Error.prototype.setNetworkError = function(message) {
   fire('error_h', message)
   this.log(message)
 }
